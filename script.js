6a3ca627607acf9ceeec46614aed76b0f8da7ff7
caristopmer <chris.mcmenomy@gmail.com>
2017-11-24 16:21:32 -0600

    jquery and nav listeners up and running.

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $('#home').on('click', function() {
+    console.log('home clicked');
+  });
+
+  $('#projects').on('click', function() {
+    console.log('projects clicked');
+  });
+
+  $('#background').on('click', function() {
+    console.log('background clicked');
+  });
+
+  $('#contact').on('click', function() {
+    console.log('contact clicked');
+  });
+
+});
