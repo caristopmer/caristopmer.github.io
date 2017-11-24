adaa88211cc4bc6ec46c8d2c58c241f01d0cc1c5
caristopmer <chris.mcmenomy@gmail.com>
2017-11-24 16:55:06 -0600

    nav scrolling working, need to fix font sizing issues.

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 $(document).ready(function() {
 
+  let navHeight = 240;
+
   $('#home').on('click', function() {
-    console.log('home clicked');
+    $('html, body').animate({
+        scrollTop: $(".home").offset().top - navHeight
+    }, 1000);
   });
 
   $('#projects').on('click', function() {
-    console.log('projects clicked');
+    $('html, body').animate({
+        scrollTop: $(".projects").offset().top - navHeight
+    }, 1000);
   });
 
   $('#background').on('click', function() {
-    console.log('background clicked');
+    $('html, body').animate({
+        scrollTop: $(".background").offset().top - navHeight
+    }, 1000);
   });
 
   $('#contact').on('click', function() {
-    console.log('contact clicked');
+    $('html, body').animate({
+        scrollTop: $(".contact").offset().top - navHeight
+    }, 1000);
   });
 
 });
